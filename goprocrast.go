ad6e00f92c3ef20659fdf803c29a9da0d0faa4f6
Daniel Jackoway <daniel@quip.com>
2020-12-31 00:12:37 -0500

    foundation wip

--- goprocrast.go
+++ goprocrast.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func dev() bool {
+	return os.Getenv("DEV") == "1" || true //temp
+}
+
+func hostsPath() string {
+	if dev() {
+		return "./hosts.dev"
+	} else {
+		return "/etc/hosts"
+	}
+}
+
+func hostsFileContent() string {
+	content, err := ioutil.ReadFile(hostsPath()) // the file is inside the local directory
+	if err != nil {
+		panic(err)
+	}
+	return string(content)
+}
+
+func activate() {
+	fmt.Println(hostsFileContent())
+}
+
+func deactivate() {
+
+}
 
 func main() {
 	fmt.Println(os.Args[0])
@@ -9,5 +40,13 @@ func main() {
 		fmt.Println("usage")
 		os.Exit(0)
 	}
-	fmt.Println(os.Args[1])
+
+	cmd := os.Args[1]
+	fmt.Println(cmd)
+	switch cmd {
+	case "on":
+		activate()
+	case "off":
+		deactivate()
+	}
 }
